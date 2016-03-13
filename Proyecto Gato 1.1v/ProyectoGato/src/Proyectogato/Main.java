3cbc207f58eab36345b241a00aa2c2a26c4cab7f
David Gutierrez <dgutierrez06@gmail.com>
2016-03-13 09:07:59 -0600

    ProyectGato
    2do Commit ProyectGato

---
+++ Proyecto Gato 1.1v/ProyectoGato/src/Proyectogato/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyectogato;
+/**
+ * @author w.sullivan-DAVID GUTIERREZ SOLANO
+ * @version Gato 1.0v
+ * @create 2016-03-06
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {    //Clase Main
+        // TODO code application logic here
+        WindowStar instanciaWindowStar=new WindowStar();    //Creacion del Object instanciaWindowStar para WindowsStar
+        instanciaWindowStar.setVisible(true);   //instanciaWindowStar vuelve true la visibilidad del Frame Principal
+    }
+}
